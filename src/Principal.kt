8f9c3efaf90b94ac43332339484fc9a3754bf92c
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2018-04-05 20:45:37 +0200

    estructura condicional if

--- src/Principal.kt
+++ src/Principal.kt
@@ -40,3 +40,23 @@ fun main(parametro: Array<String>) {
     val total = precio * cantidad
     println("El total a pagar es $total")
 } */
+
+/* PROYECTO 4
+
+fun main(parametro: Array<String>) {
+    print("Ingrese el primer valor:")
+    val valor1 = readLine()!!.toInt()
+    print("Ingrese el segundo valor:")
+    val valor2 = readLine()!!.toInt()
+    if (valor1 < valor2) {
+        val suma = valor1 + valor2
+        val resta = valor1 - valor2
+        println("La suma de los dos valores es: $suma")
+        println("La resta de los dos valores es: $resta")
+    } else {
+        val producto = valor1 * valor2
+        val division = valor1 / valor2
+        println("El producto de los dos valores es: $producto")
+        println("La división de los dos valores es: $division")
+    }
+} */
