58fd5265f743099e84121d480c3cafa969ef67d6
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2018-04-05 20:39:52 +0200

    entrada por consola

--- src/Principal.kt
+++ src/Principal.kt
@@ -3,11 +3,40 @@ fun main(parametro: Array<String>) {
     print("Hola Mundo")
 }*/
 
+/* PROYECTO 2
 fun main(parametro: Array<String>) {
+
     val valor1 = 100
     val valor2 = 400
     var resultado = valor1 + valor2
     println("La suma de $valor1 + $valor2 es $resultado")
     resultado = valor1 * valor2
     println("El producto de $valor1 * $valor2 es $resultado")
+} */
+
+/* PROYECTO 3
+fun main(argumento: Array<String>) {
+    print("Ingrese primer valor:")
+    val valor1 = readLine()!!.toInt()
+    print("Ingrese segundo valor:")
+    val valor2 = readLine()!!.toInt()
+    val suma = valor1 + valor2
+    println("La suma de $valor1 y $valor2 es $suma")
+    val producto = valor1 * valor2
+    println("El producto de $valor1 y $valor2 es $producto")}
+
+fun main(parametro: Array<String>) {
+    print("Ingrese la medida del lado del cuadrado:")
+    val lado = readLine()!!.toInt()
+    val perimetro = lado * 4
+    println("El perímetro del cuadrado es $perimetro")
 }
+
+fun main(parametro: Array<String>) {
+    print("Ingrese el precio del producto:")
+    val precio = readLine()!!.toDouble()
+    print("Ingrese la cantidad de productos:")
+    val cantidad = readLine()!!.toInt()
+    val total = precio * cantidad
+    println("El total a pagar es $total")
+} */
